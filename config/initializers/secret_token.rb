dd59d9e4f089e85e4e487cb06c49745315559f08
Granger <GrangerAbuhoff@gmail.com>
2014-05-17 13:14:40 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Surge::Application.config.secret_key_base = '8698eacebf0e43e1bb5b911e90c1572311ffeba60c31772338afee530786649ed9f45c9028879bcb87babd11f26e1cc3c79d2c5b945fb77b17e6e9a26c93e0f9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Surge::Application.config.session_store :cookie_store, key: '_surge_session'
